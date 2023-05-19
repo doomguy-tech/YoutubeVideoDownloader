bee9b044d9433d2decdda560b59fca26e358880b
Ricardo Herrera <ricardo.herrera.87@hotmail.com>
2023-05-19 15:16:39 -0500

    Add project files.

---
+++ YoutubeVideoDownloader/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace YoutubeVideoDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonDownload = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonDownloadVideo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonDownload
+            // 
+            this.buttonDownload.Location = new System.Drawing.Point(40, 156);
+            this.buttonDownload.Name = "buttonDownload";
+            this.buttonDownload.Size = new System.Drawing.Size(145, 23);
+            this.buttonDownload.TabIndex = 0;
+            this.buttonDownload.Text = "Download as MP3";
+            this.buttonDownload.UseVisualStyleBackColor = true;
+            this.buttonDownload.Click += new System.EventHandler(this.buttonDownload_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Youtube link or list :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(211, 72);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(494, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(211, 101);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 3;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(40, 343);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(665, 98);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // buttonDownloadVideo
+            // 
+            this.buttonDownloadVideo.Location = new System.Drawing.Point(211, 156);
+            this.buttonDownloadVideo.Name = "buttonDownloadVideo";
+            this.buttonDownloadVideo.Size = new System.Drawing.Size(145, 23);
+            this.buttonDownloadVideo.TabIndex = 5;
+            this.buttonDownloadVideo.Text = " Download as Video";
+            this.buttonDownloadVideo.UseVisualStyleBackColor = true;
+            this.buttonDownloadVideo.Click += new System.EventHandler(this.buttonDownloadVideo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonDownloadVideo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonDownload);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Youtube Music Downloader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonDownload;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonDownloadVideo;
+    }
+}
+
---
+++ YoutubeVideoDownloader/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YoutubeExplode.Videos;
+using YoutubeExplode;
+using System.IO;
+using YoutubeExplode.Converter;
+using YoutubeExplode.Videos.Streams;
+using YoutubeExplode.Common;
+using System.Reflection.Emit;
+using System.Text.RegularExpressions;
+
+namespace YoutubeVideoDownloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string RemoveIllegalFileNameCharacters(string fileName)
+        {
+            string illegalChars = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
+            string regexPattern = string.Format("[{0}]", Regex.Escape(illegalChars));
+            return Regex.Replace(fileName, regexPattern, "");
+        }
+
+        private async void buttonDownload_Click(object sender, EventArgs e)
+        {
+            label2.Text = string.Empty;
+            string url = textBox1.Text;
+            var youtube = new YoutubeClient();
+
+            if (url.Contains("list="))
+            {
+                var playlist = await youtube.Playlists.GetAsync(url);
+                //var playlist = await youtube.Playlists.GetAsync("https://www.youtube.com/watch?v=4X2jQXcF0S8&list=PLfou2nEbJ6Qs738XcjmsIdLtB-5SRsj8V&pp=iAQB");
+                var videos = await youtube.Playlists.GetVideosAsync(playlist.Id);
+                var currentPath = Path.Combine(Environment.CurrentDirectory,playlist.Title);
+                if (!Directory.Exists(currentPath))
+                {
+                    Directory.CreateDirectory(currentPath);
+                }
+                int count = 0;
+                label2.Text = "Downloading reproduction list...";
+                foreach (var video in videos)
+                {
+                    count++;
+                    var streamManifest = await youtube.Videos.Streams.GetManifestAsync(video.Id);
+                    var streamInfo = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
+                    var stream = await youtube.Videos.Streams.GetAsync(streamInfo);
+                    string cleanedName= RemoveIllegalFileNameCharacters( video.Title);
+                    label2.Text = $"Downloading {video.Title}, {count} of {videos.Count}";
+                    if (File.Exists(Path.Combine(currentPath, cleanedName + ".mp3")) && new FileInfo(Path.Combine(currentPath, cleanedName + ".mp3")).Length > 0)
+                    {
+                        Console.WriteLine("File exists and is not empty.");
+                    }
+                    else
+                        await youtube.Videos.Streams.DownloadAsync(streamInfo, Path.Combine(currentPath, cleanedName + ".mp3"));
+
+                }
+                label2.Text = playlist.Title+" Has been saved successfully";
+            }
+            else
+            {
+                
+
+                label2.Text = "Downloading single file...";
+                var video = await youtube.Videos.GetAsync(url);
+                //var video = await youtube.Videos.GetAsync("https://www.youtube.com/watch?v=tVUYyVfydqY");
+                var streamManifest = await youtube.Videos.Streams.GetManifestAsync(video.Id);
+                var streamInfo = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
+                var fileName = RemoveIllegalFileNameCharacters($"{video.Title}.mp3");
+                var filePath = Path.Combine(Environment.CurrentDirectory, fileName);
+                if (File.Exists(filePath) && new FileInfo(filePath).Length > 0)
+                {
+                    Console.WriteLine("File exists and is not empty.");
+                }
+                else
+                {
+                    await youtube.Videos.Streams.DownloadAsync(streamInfo, filePath);
+                    label2.Text = fileName + " has been saved successfully";
+                }
+            }
+
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBox1.Text= string.Empty;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label2.Text= string.Empty;
+        }
+
+        private async void buttonDownloadVideo_Click(object sender, EventArgs e)
+        {
+            label2.Text = string.Empty;
+            string url = textBox1.Text;
+            var youtube = new YoutubeClient();
+
+            if (url.Contains("list="))
+            {
+                var playlist = await youtube.Playlists.GetAsync(url);
+                //var playlist = await youtube.Playlists.GetAsync("https://www.youtube.com/watch?v=4X2jQXcF0S8&list=PLfou2nEbJ6Qs738XcjmsIdLtB-5SRsj8V&pp=iAQB");
+                var videos = await youtube.Playlists.GetVideosAsync(playlist.Id);
+                var currentPath = Path.Combine(Environment.CurrentDirectory, playlist.Title);
+                if (!Directory.Exists(currentPath))
+                {
+                    Directory.CreateDirectory(currentPath);
+                }
+                int count = 0;
+                label2.Text = "Downloading reproduction list...";
+                foreach (var video in videos)
+                {
+                    count++;
+                    var streamManifest = await youtube.Videos.Streams.GetManifestAsync(video.Id);
+                    var streamInfo = streamManifest.GetVideoOnlyStreams().GetWithHighestBitrate();
+                    var stream = await youtube.Videos.Streams.GetAsync(streamInfo);
+                    string cleanedName = RemoveIllegalFileNameCharacters(video.Title);
+                    label2.Text = $"Downloading {video.Title}, {count} of {videos.Count}";
+                    if (File.Exists(Path.Combine(currentPath, cleanedName + ".mp4")) && new FileInfo(Path.Combine(currentPath, cleanedName + ".mp4")).Length > 0)
+                    {
+                        Console.WriteLine("File exists and is not empty.");
+                    }
+                    else
+                        await youtube.Videos.Streams.DownloadAsync(streamInfo, Path.Combine(currentPath, cleanedName + ".mp4"));
+
+                }
+                label2.Text = playlist.Title + " Has been saved successfully";
+            }
+            else
+            {
+                label2.Text = "Downloading single file...";
+                var video = await youtube.Videos.GetAsync(url);
+                //var video = await youtube.Videos.GetAsync("https://www.youtube.com/watch?v=tVUYyVfydqY");
+                var streamManifest = await youtube.Videos.Streams.GetManifestAsync(video.Id);
+                var streamInfo = streamManifest.GetVideoOnlyStreams().GetWithHighestBitrate();
+                var fileName = RemoveIllegalFileNameCharacters($"{video.Title}.mp4");
+                var filePath = Path.Combine(Environment.CurrentDirectory, fileName);
+                if (File.Exists(filePath) && new FileInfo(filePath).Length > 0)
+                {
+                    Console.WriteLine("File exists and is not empty.");
+                }
+                else
+                {
+                    await youtube.Videos.Streams.DownloadAsync(streamInfo, filePath);
+                    label2.Text = fileName + " has been saved successfully";
+                }
+            }
+        }
+    }
+}
